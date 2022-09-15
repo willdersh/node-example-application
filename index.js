47a677d97e154c96271f3239c7001caab6a2ab89
willdersh <willdersh@gmail.com>
2022-09-14 22:06:27 -0500

    changed port

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.use(morgan('dev'))
 
 app.use('/', router)
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
